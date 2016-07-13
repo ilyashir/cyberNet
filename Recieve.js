cb1e78a870d0fcb133cc4386efd1ec27c1c05924
Dann239 <dann_pavlov@inbox.ru>
2016-07-13 10:40:28 +0300

    Перепилили прием

--- Recieve.js
+++ Recieve.js
@@ -18,13 +18,9 @@ var main = function()
 			y = [gamepad.padX(1), gamepad.padY(1)][1];
 			var x1 = [gamepad.padX(0), gamepad.padY(0)][0];
 			var y1 = [gamepad.padX(0), gamepad.padY(0)][1];
-			var a=x1%1000;
-			var xf=(x1-a)/1000;
-			brick.display().addLabel(xf,10,10);
 			brick.display().addLabel(y1,10,25);
 			brick.display().addLabel(x,10,40);
 			brick.display().addLabel(y,10,55);
-			brick.display().addLabel(a,10,70);
 			brick.display().addLabel(x1,10,85);
 			brick.display().redraw();
 		} 
