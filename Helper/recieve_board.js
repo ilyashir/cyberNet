503ce6baabb3ebaceb7ea13e831cdb588c9b86b5
Dann239 <dann_pavlov@inbox.ru>
2016-07-15 14:59:36 +0300

    Конвертируем в приемлемые форматы

--- Helper/recieve_board.js
+++ Helper/recieve_board.js
@@ -3,8 +3,6 @@ var pi = 3.1415926535897931;
 
 var main = function()
 {
-	x = 0;
-	y = 0;
 	//while (!gamepad.isConnected()) {
 	//	script.wait(10);
 	//}
@@ -12,19 +10,19 @@ var main = function()
 	while(true)
 	{
 		if (gamepad.isPadPressed(1)) {
-			x = [gamepad.padX(0), gamepad.padY(0)][0];
-			y = [gamepad.padX(0), gamepad.padY(0)][1];
-			var x1 = [gamepad.padX(1), gamepad.padY(1)][0];
-			var y1 = [gamepad.padX(1), gamepad.padY(1)][1];
-			brick.display().addLabel(x,10,25);
-			brick.display().addLabel(y,10,40);
+			var x0 = [gamepad.padX(0), gamepad.padY(0)][0]*1;
+			var y0 = [gamepad.padX(0), gamepad.padY(0)][1]*1;
+			var x1 = [gamepad.padX(1), gamepad.padY(1)][0]*1;
+			var y1 = [gamepad.padX(1), gamepad.padY(1)][1]*1;
+			print(x0);
+			brick.display().addLabel(x0,10,25);
+			brick.display().addLabel(y0,10,40);
 			brick.display().addLabel(x1,10,55);
 			brick.display().addLabel(y1,10,70);
-			brick.display().redraw();
+			brick.display().redraw();
+			if(x0>180)
+				x0 = x0 - 360;
 		}
-		var mes=1234; 
-		script.sendMessage(mes);
-		script.wait(100);
 	}
 	return;
 }
