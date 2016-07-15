57e1db593bf810f55534f7a963ac4050d8362c6d
Dann239 <dann_pavlov@inbox.ru>
2016-07-15 17:59:30 +0300

    промежуточный коммит

---
+++ BigBrother/reg.js
@@ -0,0 +1,14 @@
+var __interpretation_started_timestamp__;
+var pi = 3.1415926535897931;
+
+var main = function()
+{
+	while(true)
+	{
+		var a=brick.sensor("A4").readRawData();
+		var b=brick.sensor("A5").readRawData();
+		var v=0;
+		
+	}
+	return;
+}
