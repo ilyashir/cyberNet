80c74a00ef5b13a19b329ffffc757a3afef84021
vadim8960 <akimovvadim779@gmail.com>
2016-07-17 17:13:14 +0300

    Допилены подъезды

--- BigBrother/reg.js
+++ BigBrother/reg.js
@@ -1,11 +1,11 @@
 var __interpretation_started_timestamp__;
 var pi = 3.1415926535897931;
-var u1, u2, v = -90, k1 = 20, k2 = 20, r=4, rr;
+var u1, u2, v = 90, k1 = 10, k2 = 10, r=4, rr;
 
 var p_reg = function(){
 	var a=brick.sensor("A4").readRawData();
 	var b=brick.sensor("A5").readRawData();
-	u1=(Math.min(a,b)-600)/k1;
+	u1=(Math.min(a,b)-800)/k1;
 	u2=(-a+b)/k2;
 	brick.motor("M1").setPower(-v+u1+u2);
 	brick.motor("M2").setPower(-v+u1-u2);
@@ -34,7 +34,10 @@ var go_on_edge = function(dist)
 		a=Math.abs(brick.encoder("E1").read());
 		b=Math.abs(brick.encoder("E2").read());
 		c=Math.abs(brick.encoder("E3").read());
-		d=Math.abs(brick.encoder("E4").read());
+		d=Math.abs(brick.encoder("E4").read());
+		print("A1 = " + brick.sensor("A1").readRawData());
+		print("A2 = " + brick.sensor("A2").readRawData());
+		print("A4 = " + brick.sensor("A4").readRawData());
 	}
 	brick.motor("M1").powerOff();
 	brick.motor("M2").powerOff();
@@ -71,8 +74,10 @@ var go = function(dist)
 	brick.encoder("E2").reset();
 	brick.encoder("E3").reset();
 	brick.encoder("E4").reset();
-	var a=0,b=0,c=0,d=0;
-	while (Math.min(Math.min(a,b),Math.min(c,d))/180.0*pi*4/Math.sqrt(2.0)<dist && light < 600){
+	var a=0,b=0,c=0,d=0;
+	light = brick.sensor("A6").readRawData();
+	while (Math.min(Math.min(a,b),Math.min(c,d))/180.0*pi*4/Math.sqrt(2.0)<dist && light < 600){
+		light = brick.sensor("A6").readRawData();
 		brick.motor("M1").setPower(v);
 		brick.motor("M2").setPower(v);
 		brick.motor("M3").setPower(v);
@@ -89,8 +94,22 @@ var go = function(dist)
 }
 
 var main = function()
-{
-	rotate(360);
-	script.wait(100);
+{
+	go(20);
+	while (true){
+		print(brick.sensor("A4").readRawData());
+		go_on_edge(1000);
+		brick.motor("M1").setPower(100);
+		brick.motor("M2").setPower(100);
+		script.wait(300);
+		v = -90;
+		go_on_edge(5);
+		v = 90;
+		go(5);
+		v = -90;
+		rotate(90);
+		v = 90;
+		script.wait(100);
+	}
 	return;
 }
