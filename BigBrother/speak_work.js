ec3c17a8051d713012095ffad4125b48808ce402
vadim8960 <akimovvadim779@gmail.com>
2016-07-20 19:28:13 +0300

    добавил езду робота по столу и переход между режимами (гулять/диалог)

--- BigBrother/speak_work.js
+++ BigBrother/speak_work.js
@@ -1,8 +1,57 @@
 var pi = 3.1415926535897931;
-var end = 0, v = 90; 
-var i = 0;
-var sp_h = script.readAll("sp_human.txt");
+var end = 0;
+var u1, u2, v = 90, k1 = 10, k2 = 10, r=4, rr;
+var i = 0;
+var sp_h = script.readAll("sp_human.txt");
 var sp_t = script.readAll("sp_trik.txt");
+
+var p_reg = function(){
+	var a=brick.sensor("A4").readRawData();
+	var b=brick.sensor("A5").readRawData();
+	u1=(Math.min(a,b)-800)/k1;
+	u2=(-a+b)/k2;
+	brick.motor("M1").setPower(-v+u1+u2);
+	brick.motor("M2").setPower(-v+u1-u2);
+	brick.motor("M3").setPower(v+u1+u2);
+	brick.motor("M4").setPower(v+u1-u2);
+	script.wait(50);
+}
+
+var go_on_edge = function(dist)
+{
+	brick.encoder("E1").reset();
+	brick.encoder("E2").reset();
+	brick.encoder("E3").reset();
+	brick.encoder("E4").reset();
+	var a=0,b=0,c=0,d=0;
+	if(v>0)
+		light = brick.sensor("A1").readRawData();
+	else
+		light = brick.sensor("A2").readRawData();
+	while (Math.min(Math.min(a,b),Math.min(c,d))/180.0*pi*4/Math.sqrt(2.0)<dist && light < 800){
+		if(v>0)
+			light = brick.sensor("A1").readRawData();
+		else
+			light = brick.sensor("A2").readRawData();
+		if (brick.sensor("A3").readRawData() < 200){
+			p_reg();
+		} else {
+			end = 1;
+			break;
+		}
+		a=Math.abs(brick.encoder("E1").read());
+		b=Math.abs(brick.encoder("E2").read());
+		c=Math.abs(brick.encoder("E3").read());
+		d=Math.abs(brick.encoder("E4").read());
+		/*print("A1 = " + brick.sensor("A1").readRawData());
+		print("A2 = " + brick.sensor("A2").readRawData());
+		print("A4 = " + brick.sensor("A4").readRawData());*/
+	}
+	brick.motor("M1").powerOff();
+	brick.motor("M2").powerOff();
+	brick.motor("M3").powerOff();
+	brick.motor("M4").powerOff();
+}
 
 var mouth = function(t){
 	for(var i=0;i<t;i++){
@@ -79,25 +128,22 @@ function speak(){
 	script.system("cd scripts")
 	script.removeFile("input.txt");
 	script.writeToFile("input.txt", "");
-	
-		brick.playSound("media/beep.wav");
-		script.system("arecord -d 3 pew.wav");
-		script.wait(4000);
-		//brick.playSound("pew.wav");
-		script.system("curl -v -4 \"asr.yandex.net/asr_xml?key=d7059ba7-65ed-45d2-948e-bef1683250bb&uuid=12345678123456798734567812345678&topic=queries&lang=ru-RU\" -H \"Content-Type: audio/x-wav\" --data-binary \"@pew.wav\" | sed 's/.*confidence=\".*\">//' | sed 's#</.*##' | sed -n \"3p\" > input.txt");
-		var t = Date.now();
-		var dt = Date.now();
-		var r = 0;
-		while (lines == 0 && r < 15000){
-			lines = script.readAll("input.txt");
-			dt = Date.now();
-			script.wait(10);
-			r = dt - t;
-		}
-	lines = script.readAll("input.txt");
-	
-	
-		script.wait(50);
+		brick.playSound("media/beep.wav");
+		script.system("arecord -d 3 pew.wav");
+		script.wait(4000);
+		//brick.playSound("pew.wav");
+		script.system("curl -v -4 \"asr.yandex.net/asr_xml?key=d7059ba7-65ed-45d2-948e-bef1683250bb&uuid=12345678123456798734567812345678&topic=queries&lang=ru-RU\" -H \"Content-Type: audio/x-wav\" --data-binary \"@pew.wav\" | sed 's/.*confidence=\".*\">//' | sed 's#</.*##' | sed -n \"3p\" > input.txt");
+		var t = Date.now();
+		var dt = Date.now();
+		var r = 0;
+		while (lines == 0 && r < 15000){
+			lines = script.readAll("input.txt");
+			dt = Date.now();
+			script.wait(10);
+			r = dt - t;
+		}
+	lines = script.readAll("input.txt");
+	script.wait(50);
 	return lines[0];
 }
 
@@ -145,20 +191,69 @@ function say(text) {
 	mailbox.send(2,0);
 }
 
-var main = function(){
-	x = script.random(1, 8);
-	brick.display().showImage('pict'+x+'.png');
-	mailbox.connect("10.23.47.47");
-	script.wait(1000);
-	while (true){
+var first = function(){
+	script.wait(1000);
+	while (brick.sensor("A4").readRawData() < 1023 && brick.sensor("A5").readRawData() < 1023){
+		brick.motor("M1").setPower(-100);
+		brick.motor("M2").setPower(-100);
+		brick.motor("M3").setPower(-100);
+		brick.motor("M4").setPower(-100);
+	}
+	while (true){
+		print(brick.sensor("A4").readRawData());
+		go_on_edge(1000);
+		script.wait(300);
+		v = -90;
+		if (end == 1){
+			break;
+		}
+		go_on_edge(1000);
+		v = 90;
+		if (end == 1){
+			break;
+		}
+	}
+	end = 0;
+	brick.motor("M1").setPower(0);
+	brick.motor("M2").setPower(0);
+	brick.motor("M3").setPower(0);
+	brick.motor("M4").setPower(0);
+}
+
+var second = function(){
+	script.wait(1000);
+	while (brick.sensor("A6").readRawData() < 1023){
+		brick.motor("M1").setPower(100);
+		brick.motor("M2").setPower(100);
+		brick.motor("M3").setPower(100);
+		brick.motor("M4").setPower(100);
+	}
+	brick.motor("M1").setPower(0);
+	brick.motor("M2").setPower(0);
+	brick.motor("M3").setPower(0);
+	brick.motor("M4").setPower(0);
+	while (brick.sensor("A3").readRawData() < 200){
 		if (brick.keys().isPressed(KeysEnum.Left) == true){
 			lines = speak();
-			do_();
-		}
-		if (brick.keys().isPressed(KeysEnum.Right) == true){
-			say("меня зовут робот старший брат");
-		}
+			do_();
+		}
+		if (brick.keys().isPressed(KeysEnum.Right) == true){
+			say("меня зовут робот старший брат");
+		}
 		script.wait(100);
+	}
+}
+
+var main = function(){
+	x = script.random(1, 8);
+	brick.display().showImage('pict'+x+'.png');
+	mailbox.connect("10.23.47.30");
+	script.wait(1000);
+	while (true){
+		second();
+		script.wait(1000);
+		first();
+		script.wait(1000);
 	}
 	return;
 }
