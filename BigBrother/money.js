13c7913945192be0e2e6705eca1bfaf585b738ec
Leonov Daniil <fireman2002lds@gmail.com>
2016-07-21 15:40:08 +0300

    Программа выдачи визиток с защитой от дурака

---
+++ BigBrother/money.js
@@ -0,0 +1,15 @@
+var time0=script.time();
+var main = function()
+{
+	while(true)
+	{
+		while (!brick.keys().isPressed(KeysEnum.Up) || (script.time()-time0)<5000)script.wait(1);
+		brick.encoder(E1).reset();
+		brick.motor(M1).setPower(100);
+		while(brick.encoder(E1).read()<40) script.wait(1);
+		brick.motor(M1).setPower(0);
+		while ( brick.keys().isPressed(KeysEnum.Up))script.wait(1);
+		time0=script.time();
+	}
+	return;
+}
