2852bc0f7343d00ad0f2b2369d2c17598169223c
vadim8960 <akimovvadim779@gmail.com>
2016-07-22 09:11:16 +0300

    функция по выбрасыванию визиток

--- BigBrother/money.js
+++ BigBrother/money.js
@@ -1,15 +1,15 @@
-var time0=script.time();
+var time0=script.time();
 var main = function()
-{
-	while(true)
-	{
-		while (!brick.keys().isPressed(KeysEnum.Up) || (script.time()-time0)<5000)script.wait(1);
-		brick.encoder(E1).reset();
-		brick.motor(M1).setPower(100);
-		while(brick.encoder(E1).read()<40) script.wait(1);
-		brick.motor(M1).setPower(0);
-		while ( brick.keys().isPressed(KeysEnum.Up))script.wait(1);
-		time0=script.time();
+{
+	while(true)
+	{
+		while (!brick.keys().isPressed(KeysEnum.Up) || (script.time()-time0)<5000)script.wait(1);
+		brick.encoder(E2).reset();
+		brick.motor(M2).setPower(100);
+		while(brick.encoder(E2).read()>-40) script.wait(1);
+		brick.motor(M2).setPower(0);
+		while ( brick.keys().isPressed(KeysEnum.Up))script.wait(1);
+		time0=script.time();
 	}
 	return;
 }
